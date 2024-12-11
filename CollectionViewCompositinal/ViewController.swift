9d295f5423e08abd18bcf0bb75a77cd6afef9ef4
89Explorer <leral3@naver.com>
2024-12-11 15:01:10 +0900

    Initial Commit

---
+++ CollectionViewCompositinal/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CollectionViewCompositinal
+//
+//  Created by 권정근 on 12/11/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ CollectionViewCompositinalTests/CollectionViewCompositinalTests.swift
@@ -0,0 +1,17 @@
+//
+//  CollectionViewCompositinalTests.swift
+//  CollectionViewCompositinalTests
+//
+//  Created by 권정근 on 12/11/24.
+//
+
+import Testing
+@testable import CollectionViewCompositinal
+
+struct CollectionViewCompositinalTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
